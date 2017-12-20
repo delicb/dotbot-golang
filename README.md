ee3526cd600094898e97662cb1480ba5e420711a
Bojan Delić <bojan@delic.rs>
2017-12-20 19:44:58 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,36 @@
+Dotbot-golang
+=============
+
+Plugin for [dotbot](https://github.com/anishathalye/dotbot) that knows 
+how to install [GoLang](https://golang.org/) packages.
+
+Usage
+-----
+
+Add this repo as subrepo to your dotfiles and update `install` script.
+
+```bash
+git submodule add https://github.com/delicb/dotbot-golang
+```
+
+Then, modify install script to add path to `dotbot-golang` directory that you
+just added. Only last line is relevant (if it was not changed from default) and
+after change it might look like this:
+
+```bash
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" -p dotbot-golang "${@}"
+```
+
+To use it, add go directive. Values can be simple (only package name) or more
+detailed (dict with flags passed to `go get` command). Example:
+
+```yaml
+- go
+  - github.com/delicb/cliware
+  -
+    package: github.com/delicb/cliware-middlewares
+    flags: [-v, -u]
+    stdout: true
+    stderr: true
+```
+
---
+++ go.py
@@ -0,0 +1,86 @@
+import os
+import dotbot
+import subprocess
+import copy
+
+
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
+
+class Go(dotbot.Plugin):
+    """
+    Installs GoLang tools by using 'go get' command
+    """
+    
+    _directive = 'go'
+
+    _default_values = {
+        'package': '',
+        'flags': [],
+        'stdin': False,
+        'stdout': False,
+        'stderr': False,
+    }
+
+    _go_exec = which('go')
+
+    def can_handle(self, directive):
+        return directive == self._directive and self._go_exec
+
+    def handle(self, directive, data):
+        if not self.can_handle(directive):
+            raise ValueError(
+                'Can not handle directive %s or go no installed' % directive
+            )
+        success = True
+        for pkg_info in data:
+            data = self._apply_defaults(pkg_info)
+
+            success &= self._handle_single_package(data) == 0
+        if not success:
+            self._log.warning("Not all packages installed.")
+        else:
+            self._log.info("Finished installing go packages")
+        return success
+
+    def _handle_single_package(self, data):
+        package = data.get('package', '')
+        if not package:
+            return 0
+        with open(os.devnull, 'w') as devnull:
+            stdin = None if data.get('stdin', False) else devnull
+            stdout = None if data.get('stdout', False) else devnull
+            stderr = None if data.get('stderr', False) else devnull
+
+            flags = data.get('flags', [])
+
+            cmd = [self._go_exec, 'get'] + flags + [package]
+            self._log.warning('Running command: %s' % ' '.join(cmd))
+            ret = subprocess.call(cmd, shell=False, stdout=stdout,
+                                  stderr=stderr, stdin=stdin,
+                                  cwd=self._context.base_directory())
+            return ret
+    
+    def _apply_defaults(self, data):
+        base = copy.copy(self._default_values)
+        if isinstance(data, dict):
+            base.update(data)
+        else:
+            base.update({
+                'package': data
+            })
+        
+        return base
